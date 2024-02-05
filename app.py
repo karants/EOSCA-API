b1968c4154299159d9adf08a35494510a9253ee9
karants <karan.t.shah@outlook.com>
2024-02-04 19:43:48 -0500

    healthcheck endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #This is the EOSCA API running on Flask.
 
-#Importing Pip libraries
+#Importing pip libraries
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 #Initializing Flask instance
@@ -8,9 +8,13 @@ app = Flask(__name__)
 
 #App routes
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    return "Hello, Flask!"
+    return "Welcome to the EOSCA API"
+
+@app.route("/healthcheck/", methods=['GET'])
+def healthcheck():
+    return "I'm up and running :)"
 
 #Running the Flask instance
 if __name__ == '__main__':
