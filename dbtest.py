ce12a772aa89fb4d61178ab5113fcc6519c93e0c
karants <karan.t.shah@outlook.com>
2024-02-17 22:29:59 -0500

    dbtest revert

--- dbtest.py
+++ dbtest.py
@@ -1,7 +1,6 @@
 import pyodbc
 import os
 from dotenv import load_dotenv
-from datetime import datetime
 
 load_dotenv()
 driver=os.getenv('driver')
@@ -14,7 +13,8 @@ conn_string = 'DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+data
 
 with pyodbc.connect(conn_string) as conn:
     with conn.cursor() as cursor:
-        sql_query = 'INSERT INTO LastRefresh (Refresh_ID, Refresh_Time) VALUES (?,?)'
-        cursor.execute(sql_query, (1,datetime.now()))
-        cursor.commit()
-
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
